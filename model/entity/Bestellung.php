7dc7317fa61f9a30d15b5e7f7d04abc91dfe8413
Jannis <jannis.kirschner@gibmit.ch>
2019-01-03 16:00:40 +0100

    Update Bestellung.php

--- model/entity/Bestellung.php
+++ model/entity/Bestellung.php
@@ -9,8 +9,11 @@ class Bestellung {
 
     public static function checkEmail($email) {
         $db = Db::instantiate();
-        $query = 'SELECT id FROM kunde where deleted_at is null and email COLLATE UTF8_GENERAL_CI like "' . $email . '";';
-        $result = $db->query($query);
+        $query = 'SELECT id FROM kunde where deleted_at is null and email COLLATE UTF8_GENERAL_CI like "?";';
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("s",$email);
+        $stmt->execute();
+        $result = $stmt->get_result();
         if (!$result || $result->num_rows == 0) {
             return false;
         } else {
@@ -21,8 +24,11 @@ class Bestellung {
     public static function create($kunde_id, $targetDate) {
 //        var_dump($targetDate);
         $db = Db::instantiate();
-        $query = 'INSERT INTO bestellung (kunde_id,datum,ziel_datum) VALUES ("' . $kunde_id . '", now(),"' . $targetDate . '")';
-        $result = $db->query($query);
+        $query = 'INSERT INTO bestellung (kunde_id,datum,ziel_datum) VALUES ("?", now(),"?")';
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("is",$kunde_id,$targetDate);
+        $stmt->execute();
+        $result = $stmt->get_result();
         Db::checkConnection($result, $query);
         $last_id = $db->insert_id;
         return $last_id;
@@ -30,7 +36,11 @@ class Bestellung {
 
     public static function find($id) {
         $db = Db::instantiate();
-        $result = $db->query('SELECT * FROM bestellung WHERE deleted_at is null and id=' . $id);
+        $query=('SELECT * FROM bestellung WHERE deleted_at is null and id=?');
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("is",$kunde_id,$targetDate);
+        $stmt->execute();
+        $result = $stmt->get_result();
         //if the result is empty the $result will be an object with numrows == 0 but if its an invalid statement like "nummer="
         if (!$result || $result->num_rows == 0) {
             return false;
@@ -41,6 +51,7 @@ class Bestellung {
         }
     }
 
+    /*
     public static function findByClient($client_id) {
         $db = Db::instantiate();
         $result = $db->query('SELECT * FROM rechnung WHERE deleted_at is null and kunde_id=' . $client_id);
@@ -52,12 +63,16 @@ class Bestellung {
             $rechnung = populate::populateRechnung($rechnungArr);
             return $rechnung;
         }
-    }
+    }*/
 
     public static function allFrom($datum) {
         $rechnungen = [];
         $db = Db::instantiate();
-        $result = $db->query('SELECT * FROM rechnung WHERE deleted_at is null and datum="' . $datum . '" order by id desc');
+        $query=('SELECT * FROM rechnung WHERE deleted_at is null and datum="?" order by id desc');
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("s",$datum);
+        $stmt->execute();
+        $result = $stmt->get_result();
         while ($rechnungArr = $result->fetch_assoc()) {
             $rechnung = Populate::populateRechnung($rechnungArr);
             $rechnungen[] = $rechnung;
@@ -68,12 +83,12 @@ class Bestellung {
         return false;
     }
 
+    /*
     public static function getSearchResult($inputVal) {
         $db = Db::instantiate();
 //        echo "SELECT * FROM rechnung r INNER JOIN kunde k ON r.id = k.id WHERE k.name LIKE '%".$inputVal."%' OR k.vorname LIKE '%".$inputVal."%'";
         $result = $db->query("SELECT r.id,r.bezahlt,r.kunde_id FROM rechnung r INNER JOIN kunde k ON r.kunde_id = k.id WHERE 
         deleted_at is null and k.name LIKE '%" . $inputVal . "%' OR k.vorname LIKE '%" . $inputVal . "%'");
-        /*OR nummer LIKE '%".$inputVal."%'*/
         $rechnungen = null;
 
         while ($rechnungArr = $result->fetch_assoc()) {
@@ -84,8 +99,9 @@ class Bestellung {
             return $rechnungen;
         }
         return false;
-    }
+    }*/
 
+    /*
     public static function getRechnungBetrag($rechnung_id) {
         $db = Db::instantiate();
         $result = $db->query('SELECT sum(preis) as total FROM `position` WHERE deleted_at is null and rechnung_id=' . $rechnung_id);
@@ -96,16 +112,25 @@ class Bestellung {
             $rechnungBetrag = $result->fetch_assoc();
             return $rechnungBetrag['total'];
         }
-    }
+    }*/
 
     public static function del($id) {
         $db = Db::instantiate();
-        $query1 = 'UPDATE `bestell_position` SET deleted_at=now() WHERE bestellung_id=' . $id;
-        $query2 = 'UPDATE bestellung SET deleted_at=now() WHERE id=' . $id;
-        $sql1 = $db->query($query1);
-        $sql2 = $db->query($query2);
-        Db::checkConnection($sql1, $query1);
-        Db::checkConnection($sql2, $query2);
+        $query = 'UPDATE `bestell_position` SET deleted_at=now() WHERE bestellung_id=?';
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("i",$id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        Db::checkConnection($result, $query);
+
+
+        $query = 'UPDATE bestellung SET deleted_at=now() WHERE id=?';
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("i",$id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        Db::checkConnection($result, $query);
     }
 
     /**
@@ -113,23 +138,28 @@ class Bestellung {
      * @param $id
      * @param $value
      */
+    /*
     public static function check($id, $value) {
         $db = Db::instantiate();
         $query = 'UPDATE rechnung SET bezahlt=' . $value . ' WHERE id=' . $id;
         $sql = $db->query($query);
         Db::checkConnection($sql, $query);
-    }
+    }*/
 
     public static function updComment($id, $value) {
         $db = Db::instantiate();
-        $query = 'UPDATE rechnung SET kommentar="' . $value . '" WHERE id=' . $id;
-        $sql = $db->query($query);
-        Db::checkConnection($sql, $query);
+        $query = 'UPDATE rechnung SET kommentar="?" WHERE id=?';
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("si",$value,$id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        Db::checkConnection($result, $query);
     }
 
     /**
      * @return array|null
      */
+    /*
     public static function getDates() {
         $db = Db::instantiate();
         $result = $db->query('SELECT DISTINCT datum FROM rechnung where deleted_at is null;');
@@ -150,8 +180,9 @@ class Bestellung {
 //            $dates = $result->fetch_assoc();
             return $dates;
         }
-    }
+    }*/
 
+    /*
     public static function lastDate() {
         $db = Db::instantiate();
         $result = $db->query('SELECT datum FROM rechnung where deleted_at is null order by datum desc limit 1;');
@@ -162,7 +193,7 @@ class Bestellung {
             return date('d-m-Y', strtotime($result->fetch_assoc()['datum']));;
         }
     }
-
+*/
     /**
      * return true if there are multiple orders
      *
@@ -172,8 +203,11 @@ class Bestellung {
      */
     public static function checkMultipleOrdersAndGetOlder($kunde_id, $datum) {
         $db = Db::instantiate();
-        $query = 'select id from bestellung where kunde_id=' . $kunde_id . ' and ziel_datum="' . $datum . '" and deleted_at is null;';
-        $result = $db->query($query);
+        $query = 'select id from bestellung where kunde_id=? and ziel_datum="?" and deleted_at is null;';
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("is",$kunde_id,$datum);
+        $stmt->execute();
+        $result = $stmt->get_result();
         if (!$result || $result->num_rows == 0) {
             return false;
         }
